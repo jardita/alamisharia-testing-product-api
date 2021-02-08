69bdf24b02abf9a113f319325da5870dc628cb40
Jaka Ardita <jardita@alamisharia.co.id>
2021-02-08 23:28:24 +0700

    add endpoint getAllSeller, getSellerById

--- src/main/java/id/co/alamisharia/controller/SellerController.java
+++ src/main/java/id/co/alamisharia/controller/SellerController.java
@@ -11,11 +11,13 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class SellerController {
     @Autowired
-    private SellerService service;
+    private SellerService sellerService;
 
     @PostMapping("/addSeller")
     public ResponseEntity<ResponseEnum> addSeller(@Valid @RequestBody Seller seller, Errors errors) {
@@ -29,15 +31,56 @@ public class SellerController {
                 return new ResponseEntity<>(response, HttpStatus.OK);
             }
 
-            if (service.existsById(seller.getId()) || service.existsByNama(seller.getNama())) {
+            if (sellerService.existsById(seller.getId()) || sellerService.existsByNama(seller.getNama())) {
                 response = ResponseEnum.BAD_REQUEST;
                 response.setMessage("Seller Already Exist");
                 return new ResponseEntity<>(response, HttpStatus.OK);
             }
 
-            Seller s = service.saveSeller(seller);
+            Seller s = sellerService.saveSeller(seller);
             response = ResponseEnum.SUCCESS;
             response.setData(s);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getAllSeller")
+    public ResponseEntity<ResponseEnum> getAllSeller() {
+        try {
+            ResponseEnum response;
+
+            List<Seller> sellers = sellerService.getAllSeller();
+            if (sellers.size() < 1) {
+                response = ResponseEnum.NOT_FOUND;
+                response.setMessage("Seller Not Found");
+                return new ResponseEntity<>(response, HttpStatus.OK);
+            }
+
+            response = ResponseEnum.SUCCESS;
+            response.setData(sellers);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getSellerById")
+    public ResponseEntity<ResponseEnum> getSellerById(@RequestParam("seller_id") int sellerId) {
+        try {
+            ResponseEnum response;
+
+            if (!sellerService.existsById(sellerId)) {
+                response = ResponseEnum.NOT_FOUND;
+                response.setMessage("Seller Not Found");
+                return new ResponseEntity<>(response, HttpStatus.OK);
+            }
+
+            Optional<Seller> seller = sellerService.getSellerById(sellerId);
+
+            response = ResponseEnum.SUCCESS;
+            response.setData(seller);
             return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
--- src/main/java/id/co/alamisharia/service/SellerService.java
+++ src/main/java/id/co/alamisharia/service/SellerService.java
@@ -5,20 +5,31 @@ import id.co.alamisharia.repository.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class SellerService {
     @Autowired
-    private SellerRepository repository;
+    private SellerRepository sellerRepository;
 
     public Seller saveSeller(Seller seller) {
-        return repository.save(seller);
+        return sellerRepository.save(seller);
     }
 
     public boolean existsById(long id) {
-        return repository.existsById(id);
+        return sellerRepository.existsById(id);
     }
 
     public boolean existsByNama(String nama) {
-        return repository.existsByNama(nama);
+        return sellerRepository.existsByNama(nama);
+    }
+
+    public List<Seller> getAllSeller() {
+        return sellerRepository.findAll();
+    }
+
+    public Optional<Seller> getSellerById(long sellerId) {
+        return sellerRepository.findById(sellerId);
     }
 }
